0e9c38af23f8025a5243e0f42efb19a0fc662ef8
whitephenix13 <alexandrelasbleis@yahoo.fr>
2017-03-15 18:12:41 +0100

    2.2,2.3 done

--- main.m
+++ main.m
@@ -2,10 +2,8 @@ run('vlfeat-0.9.20/toolbox/vl_setup')
 test='BoW'; % BoW CNN
 if(strcmp(test,'BoW'))
     im = imread('Caltech4\ImageData\airplanes_test\img001.jpg');
-    %extract features 
-    [ frames, descrip ] = BoW_exctract_feature( im, 'normRGB' );
-    size(descrip)
     %build a visual vocabulary
+     visual_dic  = build_visual_vocab( 0.02, 400, 'keyPoint' );
     %quantisize feature using a visual vocabulary
     %representing image by frequencies of visual words
     %classification
