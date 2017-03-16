1b730d49f8affebc617c320851642ebeb3732faf
whitephenix13 <alexandrelasbleis@yahoo.fr>
2017-03-16 15:35:30 +0100

    add debug message

--- main.m
+++ main.m
@@ -5,16 +5,18 @@ if(strcmp(test,'BoW'))
     vocab_size = 400;
     train_percentage = 0.02;
     sift_type = 'keyPoint';
-    % 
+    %
     nb_train_each_class = 3;
     nb_test_each_class = 2;
     
     %build a visual vocabulary
+    disp('Build visual dictionnary');
     [train_descriptor_names, train_cls_names, test_cls_names] = preprocess_data(train_percentage, nb_train_each_class, nb_test_each_class);
     [visual_dic] = build_visual_vocab( train_descriptor_names, vocab_size, sift_type);
     
     %quantisize feature using a visual vocabulary.
     %randomly picked an image just to quantize and show its histogram
+    disp('Quantisize one random image to build histogram');
     perm = randperm(size(train_descriptor_names,1));
     name = num2str(cell2mat(train_descriptor_names(perm(1))));
     im = imread(strcat('Caltech4/ImageData/',name,'.jpg'));
@@ -23,32 +25,44 @@ if(strcmp(test,'BoW'))
     
     %representing image by frequencies of visual words
     % TODO : not sure whether this is the right way to plot histogram
+    disp('Show histogram of random image');
     figure
     histogram(visual_freq)
     
     %classification
-    % build X and y matrix to train an svm
-    X = zeros ([length(train_cls_names) vocab_size]);
-    y = zeros ([length(train_cls_names) 1]);
-    for i = 1: length(train_cls_names)
-        name = num2str(cell2mat(train_cls_names(i)));
-        im = imread(strcat('Caltech4/ImageData/',name,'.jpg'));
-        [f,d]= BoW_exctract_feature( im, sift_type );
-        X(i,:) = quantize_feature(visual_dic, d, vocab_size);
-        % TODO : still dirty, use aeroplane as positive label and the rest are
-        % negative label
-        if(i <= nb_train_each_class)
-            y(i) = 1;
-        else
-            y(i) = -1;
+    for obj = 1:4
+        disp(strcat('Create classifier of object ',num2str(obj),'/4'));
+        % build X(histogram of visual word) and y(positive or negative) matrix to train an svm
+        X = zeros ([length(train_cls_names) vocab_size]);
+        y = zeros ([length(train_cls_names) 1]);
+        for i = 1: length(train_cls_names)
+            name = num2str(cell2mat(train_cls_names(i)));
+            disp(strcat('Get features for_ ',name,'_step_',num2str(i),'/',num2str(length(train_cls_names)) ));
+            im = imread(strcat('Caltech4/ImageData/',name,'.jpg'));
+            [f,d]= BoW_exctract_feature( im, sift_type );
+            X(i,:) = quantize_feature(visual_dic, d, vocab_size);
+            if((obj-1) *nb_train_each_class < i && (i <= obj * nb_train_each_class))
+                y(i) = 1;
+            else
+                y(i) = -1;
+            end
+        end
+        
+        %training the model
+        if(obj == 1) 
+            SVMModel_airp = fitcsvm(X,y);
+        elseif(obj==2)
+            SVMModel_cars = fitcsvm(X,y);
+        elseif(obj==3)
+            SVMModel_faces = fitcsvm(X,y);
+        elseif(obj==4)
+            SVMModel_motor = fitcsvm(X,y);
         end
     end
     
-    %training the model
-    SVMModel = fitcsvm(X,y);
-    
-    %testing: 
+    %testing:
     %build matrix for testing
+    disp(strcat('Test classifier ','1','/4'));
     X_test = zeros ([length(test_cls_names) vocab_size]);
     for i = 1: length(test_cls_names)
         name = num2str(cell2mat(test_cls_names(i)));
@@ -57,16 +71,16 @@ if(strcmp(test,'BoW'))
         X_test(i,:) = quantize_feature(visual_dic, d, vocab_size);
     end
     
-    label = predict(SVMModel,X_test);
+    label = predict(SVMModel_airp,X_test);
     
     %compute Mean average precision
-    
+    disp('Compute MaP');
 elseif(strcmp(test,'CNN'))
     %define network architecture
-    %Data preprocessing 
-    %Data feeding 
+    %Data preprocessing
+    %Data feeding
     
-    %testing: 
+    %testing:
     %visualize feature space
     %evaluate accuracy
 end
--- quantize_feature.m
+++ quantize_feature.m
@@ -1,9 +1,16 @@
 function [visual_freq] = quantize_feature(visual_dic, descriptor, vocab_size)
 visual_freq = zeros([vocab_size 1]);
+%for all the descriptor in the image 
+total_num = size(descriptor,2) * size(visual_dic,1);
 for i=1:size(descriptor,2)
     min_distance = 1000000000;
     descriptor_centroid = 0;
+    %look for the the closest visual word for this descriptor 
     for j=1:size(visual_dic,1)
+        current_ind = i + (j-1) * size(descriptor,2);
+        if(mod(current_ind,10000)==0)
+            %disp(strcat('step_ ', num2str(current_ind) , '/', num2str(total_num)));
+        end
         dist = pdist2(double(descriptor(:,i)'),double(visual_dic(j,:)));
         if(dist < min_distance)
             min_distance = dist;
